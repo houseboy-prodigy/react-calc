4f104038f71178724b9275fdd4f159e07d48834d
houseboy-prodigy <59971620+houseboy-prodigy@users.noreply.github.com>
2023-08-20 16:16:21 -0500

    working calculator, still need to finish nuances like decimals, %, inverse, unittesting

--- src/App.js
+++ src/App.js
@@ -9,27 +9,38 @@ function App() {
   const [prevNum,setPrevNum] = useState(null)
   const [operand,setOperand] = useState(null)
   const numClickHandler = (num) => {
-  if(!currentNum){
-    setCurrentNum(num)
-  }
-  else if(currentNum&&!operand){
-    let newNum = currentNum.toString()+num.toString()
-    setCurrentNum(parseInt(newNum))
-  }
-  else{
-    setPrevNum(currentNum)
-    setCurrentNum(num)
-  }
+    if(!currentNum){
+      setCurrentNum(num)
+    }
+    else{
+      setCurrentNum(currentNum+num)
+    }
   }
 
-  const opHandler = () => {
+  const opHandler = (op) => {
     if(currentNum){
-      setOperand("*")
+      setOperand(op)
+      setPrevNum(currentNum)
+      setCurrentNum(null)
     }
   }
   const evalExp = () => {
     if(currentNum&&prevNum&&operand){
-      setCurrentNum(prevNum*currentNum)
+      switch(operand){
+        case '+':
+      setCurrentNum(parseInt(prevNum) + parseInt(currentNum));
+      break;
+    case '-':
+      setCurrentNum(parseInt(prevNum) - parseInt(currentNum));
+      break;
+    case '*':
+      setCurrentNum(parseInt(prevNum) * parseInt(currentNum));
+      break;
+    case '/':
+      setCurrentNum(prevNum / currentNum);
+      break;
+      }
+    
     }
   }
 
@@ -41,26 +52,26 @@ function App() {
     return (
     <div className="container">
     <div className="mainscreen">
-    <text className="maintext">{currentNum||0}</text>
+    <text className="maintext">{currentNum||prevNum||0}</text>
     </div>
     <div className="numButtonContainer">
     <CalcButton value="AC" onClickHandler={()=> resetCalc()}type="firstRowButtons"/>
     <CalcButton value="+/-" type="firstRowButtons"/>
     <CalcButton value="%" type="firstRowButtons"/>
-    <CalcButton value="/" type="operandButtons"/>
-      <CalcButton value="7" onClickHandler={() => numClickHandler(7)}/>
-      <CalcButton value="8" onClickHandler={() => numClickHandler(8)}/>
-      <CalcButton value="9" onClickHandler={() => numClickHandler(9)}/>
+    <CalcButton value="/" onClickHandler={() => opHandler("/")} type="operandButtons"/>
+      <CalcButton value="7" onClickHandler={() => numClickHandler("7")}/>
+      <CalcButton value="8" onClickHandler={() => numClickHandler("8")}/>
+      <CalcButton value="9" onClickHandler={() => numClickHandler("9")}/>
       <CalcButton value="X" onClickHandler={() => opHandler("*")} type="operandButtons"/>
-      <CalcButton value="4" onClickHandler={() => numClickHandler(4)}/>
-      <CalcButton value="5" onClickHandler={() => numClickHandler(5)}/>
-      <CalcButton value="6" onClickHandler={() => numClickHandler(6)}/>
-      <CalcButton value="-" type="operandButtons"/>
-      <CalcButton value="1" onClickHandler={() => numClickHandler(1)}/>
-      <CalcButton value="2" onClickHandler={() => numClickHandler(2)}/>
-      <CalcButton value="3" onClickHandler={() => numClickHandler(3)}/>
-      <CalcButton value="+" type="operandButtons"/>
-      <CalcButton value="0" onClickHandler={() => numClickHandler(0)} type="zeroStyle"/>
+      <CalcButton value="4" onClickHandler={() => numClickHandler("4")}/>
+      <CalcButton value="5" onClickHandler={() => numClickHandler("5")}/>
+      <CalcButton value="6" onClickHandler={() => numClickHandler("6")}/>
+      <CalcButton value="-" onClickHandler={() => opHandler("-")} type="operandButtons"/>
+      <CalcButton value="1" onClickHandler={() => numClickHandler("1")}/>
+      <CalcButton value="2" onClickHandler={() => numClickHandler("2")}/>
+      <CalcButton value="3" onClickHandler={() => numClickHandler("3")}/>
+      <CalcButton value="+" onClickHandler={() => opHandler("+")} type="operandButtons"/>
+      <CalcButton value="0" onClickHandler={() => numClickHandler("0")} type="zeroStyle"/>
       <CalcButton value="."/>
       <CalcButton value="=" onClickHandler={() => evalExp()}type="operandButtons"/>
       </div>
